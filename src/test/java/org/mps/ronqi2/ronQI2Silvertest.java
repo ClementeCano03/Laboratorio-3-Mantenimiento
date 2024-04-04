c5974ca34ba5c23b4c4887070f3f055da21310a9
Cano <clementecano2003@gmail.com>
2024-04-04 19:50:40 +0200

    Test Método Reconectar

--- src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
+++ src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
@@ -5,8 +5,10 @@
  */
 package org.mps.ronqi2;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
@@ -24,22 +26,6 @@ public class ronQI2Silvertest {
      * debería devolver true. En cualquier otro caso false. Se deja programado un ejemplo.
      */
     
-    @Test
-    @DisplayName("Si es posible conectar ambos sensores y configurarlos, inicializar debe devolver true")
-    public void inicializar_WithSensorsWorksCorrectly_ReturnTrue(){
-        RonQI2 ronqi2 = new RonQI2Silver();
-
-        Dispositivo mockedDispositivo = mock(DispositivoSilver.class);
-        when(mockedDispositivo.conectarSensorPresion()).thenReturn(true);
-        when(mockedDispositivo.conectarSensorSonido()).thenReturn(true);
-        when(mockedDispositivo.configurarSensorPresion()).thenReturn(true);
-        when(mockedDispositivo.configurarSensorSonido()).thenReturn(true);
-        ronqi2.anyadirDispositivo(mockedDispositivo);
-
-        boolean result = ronqi2.inicializar();
-
-        assertTrue(result);
-    }
 
     /*
      * Un inicializar debe configurar ambos sensores, comprueba que cuando se inicializa de forma correcta (el conectar es true), 
@@ -53,7 +39,7 @@ public class ronQI2Silvertest {
      */
     @Test
     @DisplayName("Si no esta conectado, reconectamos el dispositivo")
-    public void reconectar_Device_ReturnTrue(){
+    public void reconectar_DeviceWithCorrectValues_ReturnTrue(){
         RonQI2 ronqi2 = new RonQI2Silver();
 
         Dispositivo mockedDispositivo = mock(DispositivoSilver.class);
@@ -66,6 +52,71 @@ public class ronQI2Silvertest {
         boolean result = ronqi2.reconectar();
 
         assertTrue(result);
+
+        verify(mockedDispositivo).estaConectado(); //si no se pone times(), se entiende que es una sola vez
+        verify(mockedDispositivo).conectarSensorSonido();
+        verify(mockedDispositivo).conectarSensorPresion();
+
+    }
+
+    @Test
+    @DisplayName("Si no esta conectado, reconectamos el dispositivo, pero falla al conectar el sensor de sonido")
+    public void reconectar_DeviceFailSoundSensor_ReturnFalse(){
+        RonQI2 ronqi2 = new RonQI2Silver();
+
+        Dispositivo mockedDispositivo = mock(DispositivoSilver.class);
+        when(mockedDispositivo.estaConectado()).thenReturn(false);
+        when(mockedDispositivo.conectarSensorSonido()).thenReturn(false);
+        when(mockedDispositivo.conectarSensorPresion()).thenReturn(true);
+
+        ronqi2.anyadirDispositivo(mockedDispositivo);
+
+        boolean result = ronqi2.reconectar();
+
+        assertFalse(result);
+
+        verify(mockedDispositivo).estaConectado(); 
+        verify(mockedDispositivo).conectarSensorSonido();
+        verify(mockedDispositivo).conectarSensorPresion();
+
+    }
+
+    @Test
+    @DisplayName("Si no esta conectado, reconectamos el dispositivo, pero falla al conectar el sensor de presión")
+    public void reconectar_DeviceFailPressureSensor_ReturnFalse(){
+        RonQI2 ronqi2 = new RonQI2Silver();
+
+        Dispositivo mockedDispositivo = mock(DispositivoSilver.class);
+        when(mockedDispositivo.estaConectado()).thenReturn(false);
+        when(mockedDispositivo.conectarSensorPresion()).thenReturn(false);
+
+        ronqi2.anyadirDispositivo(mockedDispositivo);
+
+        boolean result = ronqi2.reconectar();
+
+        assertFalse(result);
+
+        verify(mockedDispositivo).estaConectado();
+        verify(mockedDispositivo).conectarSensorPresion();
+
+    }
+
+    @Test
+    @DisplayName("Si no esta conectado, reconectamos el dispositivo, pero falla al conectar el sensor de presión")
+    public void reconectar_DeviceIsConected_ReturnFalse(){
+        RonQI2 ronqi2 = new RonQI2Silver();
+
+        Dispositivo mockedDispositivo = mock(DispositivoSilver.class);
+        when(mockedDispositivo.estaConectado()).thenReturn(true);
+
+        ronqi2.anyadirDispositivo(mockedDispositivo);
+
+        boolean result = ronqi2.reconectar();
+
+        assertFalse(result);
+
+        verify(mockedDispositivo).estaConectado();
+
     }
     
     /*
